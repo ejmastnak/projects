721ffd81bfb604bb28204d6a2b9be9dc62dcec3a
Elijan Mastnak <git@ejmastnak.com>
2025-08-28 18:11:41 +0200

    fix: remove nonexistent LJ16 MiniOffice route

--- src/router.js
+++ src/router.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/Pages/Home.vue'
-import MiniOffice from '@/Pages/LJ16/MiniOffice.vue'
 import AtticGym from '@/Pages/TV47/AtticGym.vue'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/lj16/minioffice', component: MiniOffice },
   { path: '/tv47/attic-gym', component: AtticGym },
 ]
 
