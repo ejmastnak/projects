09c78e3da0c90434514ac3fa99d84125422e8682
Elijan Mastnak <git@ejmastnak.com>
2025-09-08 10:10:34 +0200

    refactor: pass routes instead of router to createApp
    Since vite-ssg takes a list of routes instead of a router instance.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
-import Home from '@/Pages/Home.vue'
-import AtticGym from '@/Pages/TV47/AtticGym.vue'
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/tv47/attic-gym', component: AtticGym },
-]
+import routes from './routes'
 
 export const createApp = ViteSSG(
   App,
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/Pages/Home.vue'
 import AtticGym from '@/Pages/TV47/AtticGym.vue'
 
@@ -7,9 +6,4 @@ const routes = [
   { path: '/tv47/attic-gym', component: AtticGym },
 ]
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
-
-export default router
+export default routes
