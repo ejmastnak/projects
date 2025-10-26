8b38b3988bc9d9e821c8f83987342a55758496f1
Elijan Mastnak <git@ejmastnak.com>
2025-10-26 15:52:13 +0100

    build: uninstall astro tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   vite: {
@@ -9,5 +8,5 @@ export default defineConfig({
       },
     },
   },
-  integrations: [tailwind()],
+  integrations: [],
 });
